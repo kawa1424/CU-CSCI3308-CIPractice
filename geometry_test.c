f7014720f1da41620be0138c2b2df84ea693d0cf
kawa1424 <43619946+kawa1424@users.noreply.github.com>
2018-11-28 16:29:12 -0700

    Update geometry_test.c

--- geometry_test.c
+++ geometry_test.c
@@ -182,7 +182,7 @@ Suite* coord_2d_suite(void)
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
     
     TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triagnle");
-    tcase_add_test(tc_2d_area_triangle,tc_2d_area_triangle,tc_2d_area_triangle);
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
 
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
